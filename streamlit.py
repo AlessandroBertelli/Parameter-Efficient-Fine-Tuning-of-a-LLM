c918aa4a8b91f2c2bf52013964ebe5e66cbde462
Nicolò Gandini <126022150+nicologandini@users.noreply.github.com>
2025-12-05 17:28:30 +0100

    update langchain library

--- streamlit.py
+++ streamlit.py
@@ -8,7 +8,7 @@ import tempfile
 # --- LIBRERIE RAG ---
 from langchain_community.document_loaders import TextLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
 # --- CONFIGURAZIONE ---
